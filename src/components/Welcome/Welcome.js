c59abe6a6a49fc595a90909fb93c76bb0545ee0f
thejustinlombardi <thejustinlombardi@gmail.com>
2022-03-10 18:18:58 -0500

    Edit welcome page

--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -5,14 +5,15 @@ import styles from "./Welcome.module.css";
 function Welcome() {
 	return (
 		<div className={styles.welcome_container}>
-			<Link to="home" className={styles.welcome_link}>
-				Start reading
-			</Link>
+			<div className={styles.welcome_room}></div>
 			<img
 				className={styles.welcome_img}
 				src="https://i.imgur.com/6hVEsYW.png"
 				alt="logo"
 			/>
+			<Link to="home" className={styles.welcome_link}>
+				Start reading
+			</Link>
 		</div>
 	);
 }
