6d39347353bf1fb796e87d942d489276f315e760
thejustinlombardi <thejustinlombardi@gmail.com>
2022-03-12 16:52:56 -0500

    Add layout

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 function Home(props) {
-	return <div></div>;
+	return (
+		<div>
+			{" "}
+			<h2>Daily Find</h2>
+			Go to section - dark - love - misc
+		</div>
+	);
 }
 
 export default Home;
