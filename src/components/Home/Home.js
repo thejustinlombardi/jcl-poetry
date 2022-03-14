63269390c1872b1e7ff9fa181839521ad2e88376
thejustinlombardi <thejustinlombardi@gmail.com>
2022-03-13 21:41:44 -0400

    Add first poem

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,7 +5,12 @@ function Home(props) {
 		<div>
 			{" "}
 			<h2>Daily Find</h2>
-			Go to section - dark - love - misc
+			<p>
+				I wrote something down, but then I erased it. I've done this about
+				twenty times now. I had an idea, then I got overwhelmed by the voices in
+				my head. They've been screaming; I'm being haunted. Most days I can shut
+				them up, but today I can't. Some days I'm okay, but today I'm not.
+			</p>
 		</div>
 	);
 }
