86f9a3baf0b3168855bfb68c1e5adc227ae5e16d
thejustinlombardi <thejustinlombardi@gmail.com>
2022-03-14 21:13:07 -0400

    Add home stylesheet / Add style to daily poem

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,16 +1,20 @@
 import React from "react";
 
+import styles from "./Home.module.css";
+
 function Home(props) {
 	return (
-		<div>
+		<div className={styles.daily_poem}>
 			{" "}
 			<h2>Daily Find</h2>
-			<p>
-				I wrote something down, but then I erased it. I've done this about
-				twenty times now. I had an idea, then I got overwhelmed by the voices in
-				my head. They've been screaming; I'm being haunted. Most days I can shut
-				them up, but today I can't. Some days I'm okay, but today I'm not.
-			</p>
+			<p>I wrote something down, but then I erased it.</p>
+			<p>I've done this about twenty times now. </p>
+			<p>I had an idea, then I got overwhelmed by the voices in my head. </p>
+			<p>They've been screaming; I'm being haunted.</p>
+			<p>Most days I can shut them up, </p>
+			<p>but today I can't. </p>
+			<p>Some days I'm okay, </p>
+			<p>but today I'm not.</p>
 		</div>
 	);
 }
