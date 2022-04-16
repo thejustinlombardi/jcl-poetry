865715e911c431a5019f9c3ca88c6de946bfeaba
thejustinlombardi <thejustinlombardi@gmail.com>
2022-04-16 11:36:55 -0400

    Edit home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,29 +6,19 @@ import styles from "./Home.module.css";
 function Home(props) {
 	return (
 		<div className={styles.daily_poem}>
-			{" "}
-			<h2>Daily Find</h2>
-			<p>I wrote something down, but then I erased it.</p>
-			<p>I've done this about twenty times now. </p>
-			<p>I had an idea, then I got overwhelmed by the voices in my head. </p>
-			<p>They've been screaming; I'm being haunted.</p>
-			<p>Most days I can shut them up, </p>
-			<p>but today I can't. </p>
-			<p>Some days I'm okay, </p>
-			<p>but today I'm not.</p>
 			<Link to={"/love-poems"}>
-				<div>
-					<img src="" alt="" />
+				<div className={styles.img_links}>
+					<img src="https://i.imgur.com/i9oXitV.png" alt="" />
 				</div>
 			</Link>
 			<Link to={"/dark-poems"}>
-				<div>
-					<img src="" alt="" />
+				<div className={styles.img_links}>
+					<img src="https://i.imgur.com/FrGFT8N.png" alt="" />
 				</div>
 			</Link>
 			<Link to={"/misc-poems"}>
-				<div>
-					<img src="" alt="" />
+				<div className={styles.img_links}>
+					<img src="https://i.imgur.com/FrGFT8N.png" alt="" />
 				</div>
 			</Link>
 		</div>
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,39 +1,7 @@
-import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
-
 import styles from "./Navigation.module.css";
 
 function Navigation(props) {
-	return (
-		<div className={styles.nav_container}>
-			<Navbar expand="lg">
-				<Container>
-					<Navbar.Brand href="home">
-						<img
-							src="https://i.imgur.com/vgpwK15.png"
-							width="70"
-							height="70"
-							className="d-inline-block align-top"
-							alt="JCL Logo"
-						/>
-					</Navbar.Brand>
-					<Navbar.Toggle aria-controls="basic-navbar-nav" />
-					<Navbar.Collapse id="basic-navbar-nav">
-						<Nav className="me-auto">
-							<Nav.Link href="home">Home</Nav.Link>
-							<Nav.Link href="contact">Contact</Nav.Link>
-							<NavDropdown title="Poems" id="basic-nav-dropdown">
-								<NavDropdown.Item href="love-poems">Love</NavDropdown.Item>
-								<NavDropdown.Item href="dark-poems">Dark</NavDropdown.Item>
-								<NavDropdown.Item href="misc-poems">
-									Miscellaneous
-								</NavDropdown.Item>
-							</NavDropdown>
-						</Nav>
-					</Navbar.Collapse>
-				</Container>
-			</Navbar>
-		</div>
-	);
+	return <div className={styles.nav_container}></div>;
 }
 
 export default Navigation;
