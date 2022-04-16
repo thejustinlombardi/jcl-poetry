c1bfece73b1d209b20586e20def0acf438e14e9d
thejustinlombardi <thejustinlombardi@gmail.com>
2022-04-16 11:51:52 -0400

    Edit Love div

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,18 +6,19 @@ import styles from "./Home.module.css";
 function Home(props) {
 	return (
 		<div className={styles.daily_poem}>
-			<Link to={"/love-poems"}>
-				<div className={styles.img_links}>
-					<img src="https://i.imgur.com/i9oXitV.png" alt="" />
+			<Link to={"/love-poems"} className={styles.img_links}>
+				<div>
+					<img src="https://i.imgur.com/i9oXitV.png" alt="love-poems" />
+					<h2 className={styles.love_text}>LOVE</h2>
 				</div>
 			</Link>
-			<Link to={"/dark-poems"}>
-				<div className={styles.img_links}>
+			<Link to={"/dark-poems"} className={styles.img_links}>
+				<div>
 					<img src="https://i.imgur.com/FrGFT8N.png" alt="" />
 				</div>
 			</Link>
-			<Link to={"/misc-poems"}>
-				<div className={styles.img_links}>
+			<Link to={"/misc-poems"} className={styles.img_links}>
+				<div>
 					<img src="https://i.imgur.com/FrGFT8N.png" alt="" />
 				</div>
 			</Link>
