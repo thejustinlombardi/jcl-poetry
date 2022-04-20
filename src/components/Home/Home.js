29d148a3eb2a52dc1e0512a1020788772c5bcbe8
thejustinlombardi <thejustinlombardi@gmail.com>
2022-04-20 19:18:32 -0400

    Add new fonts / edit home and welcome pages

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,23 +5,28 @@ import styles from "./Home.module.css";
 
 function Home(props) {
 	return (
-		<div className={styles.daily_poem}>
-			<Link to={"/love-poems"} className={styles.img_links}>
-				<div>
-					<img src="https://i.imgur.com/i9oXitV.png" alt="love-poems" />
-					<h2 className={styles.love_text}>LOVE</h2>
-				</div>
-			</Link>
-			<Link to={"/dark-poems"} className={styles.img_links}>
-				<div>
-					<img src="https://i.imgur.com/FrGFT8N.png" alt="" />
-				</div>
-			</Link>
-			<Link to={"/misc-poems"} className={styles.img_links}>
-				<div>
-					<img src="https://i.imgur.com/FrGFT8N.png" alt="" />
-				</div>
-			</Link>
+		<div>
+			<div className={styles.home_h1}>
+				<h1>JCL POETRY</h1>
+			</div>
+			<div className={styles.daily_poem}>
+				<Link to={"/love-poems"} className={styles.img_links}>
+					<div>
+						<img src="https://i.imgur.com/i9oXitV.png" alt="love-poems" />
+						<h2 className={styles.love_text}>LOVE</h2>
+					</div>
+				</Link>
+				<Link to={"/dark-poems"} className={styles.img_links}>
+					<div>
+						<img src="https://i.imgur.com/FrGFT8N.png" alt="" />
+					</div>
+				</Link>
+				<Link to={"/misc-poems"} className={styles.img_links}>
+					<div>
+						<img src="https://i.imgur.com/FrGFT8N.png" alt="" />
+					</div>
+				</Link>
+			</div>
 		</div>
 	);
 }
